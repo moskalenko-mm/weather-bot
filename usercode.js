6708f16992aedf09b91464b8df3dc4bbed2a4b82
Nicholas <57230936+moskalenko-mm@users.noreply.github.com>
2026-02-05 23:27:02 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,65 +1,81 @@
-module.exports = (data) => {
-    var action = data.action;
+module.exports = function(data) {
+    var action = data.action; 
     var lang = data.lang || 'ua';
+    
     var keyName = (lang === 'en') ? 'nameUS' : 'nameUA';
-
+    
+    var sortLocale = (lang === 'en') ? 'en' : 'uk';
 
     if (action === 'get_config') {
-        var config = require('./question_config.json');
-        return { "result": config[lang] || config['ua'] };
+        try {
+            var config = require('./question_config.json');
+            return { "result": config[lang] || config['ua'] };
+        } catch (e) { return { "error": "Config not found" }; }
     }
-
 
     if (action === 'get_letters') {
         var countries = require('./country.json');
         var letters = [];
         
         countries.forEach(function(c) {
-            var firstChar = c[keyName].charAt(0).toUpperCase();
-            if (letters.indexOf(firstChar) === -1) letters.push(firstChar);
+            var name = c[keyName];
+            if (name) {
+                var firstChar = name.charAt(0).toUpperCase();
+                if (letters.indexOf(firstChar) === -1) letters.push(firstChar);
+            }
         });
-        letters.sort();
+        
+        letters.sort(function(a, b) {
+            return a.localeCompare(b, sortLocale);
+        });
+        
         return { "letters": letters };
     }
-
 
     if (action === 'get_countries') {
         var countries = require('./country.json');
         var letter = data.letter; 
         
         var filtered = countries.filter(function(c) {
-            return c[keyName].startsWith(letter);
+            return c[keyName] && c[keyName].startsWith(letter);
         });
 
         var result = filtered.map(function(c) {
             return { "name": c[keyName], "code": c.code };
         });
+        
+        result.sort(function(a, b) {
+            return a.name.localeCompare(b.name, sortLocale);
+        });
+
         return { "countries": result };
     }
-
 
     if (action === 'get_city_letters') {
         var cities = require('./city.json');
         var countryCode = data.country_code;
         var cityLetters = [];
 
-    
         var countryCities = cities.filter(function(city) {
             return city.code === countryCode;
         });
 
-   
         countryCities.forEach(function(city) {
-            var firstChar = city[keyName].charAt(0).toUpperCase();
-            if (cityLetters.indexOf(firstChar) === -1) {
-                cityLetters.push(firstChar);
+            var name = city[keyName];
+            if (name) {
+                var firstChar = name.charAt(0).toUpperCase();
+                if (cityLetters.indexOf(firstChar) === -1) {
+                    cityLetters.push(firstChar);
+                }
             }
         });
         
-        cityLetters.sort();
+        cityLetters.sort(function(a, b) {
+            return a.localeCompare(b, sortLocale);
+        });
+        
         return { "letters": cityLetters };
     }
-
 
     if (action === 'get_cities') {
         var cities = require('./city.json');
@@ -67,19 +83,22 @@ module.exports = (data) => {
         var letter = data.letter;
 
         var filtered = cities.filter(function(city) {
-
-            return city.code === countryCode && city[keyName].startsWith(letter);
+            return city.code === countryCode && city[keyName] && city[keyName].startsWith(letter);
         });
 
         var result = filtered.map(function(city) {
             return {
                 "name": city[keyName],
-                "country_code": city.code 
+                "country_code": city.code
             };
+        });
+        
+        result.sort(function(a, b) {
+            return a.name.localeCompare(b.name, sortLocale);
         });
 
         return { "cities": result };
     }
 
-    return { "error": "Unknown action" };
+    return { "error": "Unknown action: " + action };
 };
