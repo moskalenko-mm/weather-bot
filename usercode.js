54fe44bdd989720fe56084dd1096d590f569ba78
Nicholas <57230936+moskalenko-mm@users.noreply.github.com>
2026-02-08 19:32:46 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -4,27 +4,23 @@ module.exports = function(data) {
     var action = data.action;
     var lang = data.lang || 'ua';
     
-    // Налаштування для сортування
+    // Визначаємо ключі для мови
     var keyName = (lang === 'en') ? 'nameUS' : 'nameUA';
     var sortLocale = (lang === 'en') ? 'en' : 'uk';
 
     try {
         if (action === 'get_config') {
-            // Завантажуємо конфіг
             var configPath = path.join(__dirname, 'question_config.json');
             var rawConfig = require(configPath);
-
             var conf = rawConfig[lang] || rawConfig['ua'];
-
+            
             data.config = {
                 txt_start: conf.start_msg,
                 txt_select_country: conf.select_country_msg,
                 txt_select_city: conf.select_city_msg,
                 txt_date: conf.select_date_msg,
                 txt_time: conf.select_time_msg,
-                txt_weather: conf.weather_msg,
-                txt_country_letter: conf.select_country_letter_msg,
-                txt_city_letter: conf.select_city_letter_msg
+                txt_weather: conf.weather_msg
             };
 
         } else if (action === 'get_letters') {
@@ -57,14 +53,29 @@ module.exports = function(data) {
             data.countries = result;
 
         } else if (action === 'get_city_letters') {
+            // 1. Спочатку знайдемо назву країни
+            var countryPath = path.join(__dirname, 'country.json');
+            var countries = require(countryPath);
+            var countryCode = data.country_code;
+
+            // Шукаємо країну
+            var currentCountry = countries.find(function(c) {
+                return c.code === countryCode;
+            });
+
+            if (currentCountry) {
+                data.country_name = currentCountry[keyName];
+            }
+
+
             var cityPath = path.join(__dirname, 'city.json');
             var cities = require(cityPath);
-            var countryCode = data.country_code;
             var cityLetters = [];
 
             var countryCities = cities.filter(function(city) {
                 return city.code === countryCode;
             });
+
             countryCities.forEach(function(city) {
                 var name = city[keyName];
                 if (name) {
@@ -72,6 +83,7 @@ module.exports = function(data) {
                     if (cityLetters.indexOf(firstChar) === -1) cityLetters.push(firstChar);
                 }
             });
+            
             cityLetters.sort(function(a, b) { return a.localeCompare(b, sortLocale); });
             data.letters = cityLetters;
 
